f412ad6a1fa63fcf2e45f1d83887da3f74081d0b
alysoncp <alyson.c.pickett@gmail.com>
2020-11-05 22:34:36 +0000

    added geolocating api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, response, ip) => {
+/*fetchMyIP((error, response, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -11,7 +11,19 @@ fetchMyIP((error, response, ip) => {
     console.log('It worked! Returned IP:' , ip);
   } else {
     console.log(`No go! Error code: ${response}`);
-  }  
+  }
   
   return
+});
+*/
+
+
+fetchCoordsByIP('66.183.219.115', (error, data) => {
+  if (data.status === 'fail') {
+    console.log(data.message);
+  } else {
+    console.log(data.lat);
+    console.log(data.lon);
+  }
+
 });
--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   const requestString = 'https://api.ipify.org?format=json';
   request(requestString, (error, response, body) => {
     if (error !== null) {
@@ -11,6 +11,17 @@ const fetchMyIP = function(callback) {
     }
  
   });
-};  
+};
 
-module.exports = { fetchMyIP } ;
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    callback(response.statusCode, data);
+  
+ 
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
